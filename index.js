78c12cfca4c4ec884154c31b492edd40fafee8ab
Ron <raunaqbansal@outlook.com>
2022-06-14 20:33:50 +1200

    finished win condition detection

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ boxesArray.forEach(function (box) {
     let clicked_box = box.getAttribute("value");
     // validateMove(box);
     if (!validateMove(box)) {
-        return;
+      return;
     }
     insertPiece(box);
     updateClaimedGrid(box);
-    checkVictory();
+    if (checkVictory()) {
+      console.log("HEY WE HAVE A WINNER!");
+      return;
+    }
+    // checkVictory();
     updateTurn(box);
   });
 });
@@ -32,13 +36,9 @@ function validateMove(box) {
   let num_Os = existing_pieces.split("o").length - 1;
   if (num_Xs == 3) {
     console.log("this square has been fully claimed, please pick another");
-    // updateClaimedGrid(box, 1);
-    // updateTurn(box);
     return false;
   } else if (num_Os == 3) {
     console.log("this square has been fully claimed, please pick another");
-    // updateClaimedGrid(box, 2);
-    // updateTurn(box);
     return false;
   } else {
     return true;
@@ -59,11 +59,25 @@ function insertPiece(box) {
 
 function updateClaimedGrid(box) {
   let playerNum = currentTurn ? "1" : "2";
+  let existing_pieces = box.innerText;
+
   let box_row = box.getAttribute("value")[0];
   let box_col = box.getAttribute("value")[1];
 
-  claimedGridArray[box_row][box_col] = playerNum;
-  // console.log(claimedGridArray);
+  let num_Xs = existing_pieces.split("x").length - 1;
+  let num_Os = existing_pieces.split("o").length - 1;
+  if (num_Xs == 3) {
+    claimedGridArray[box_row][box_col] = playerNum;
+    console.log("this square has been fully claimed, please pick another");
+    return;
+  } else if (num_Os == 3) {
+    claimedGridArray[box_row][box_col] = playerNum;
+    console.log("this square has been fully claimed, please pick another");
+    return;
+  }
+
+  // claimedGridArray[box_row][box_col] = playerNum;
+  console.log(claimedGridArray);
 }
 
 function updateTurn(box) {
@@ -73,5 +87,39 @@ function updateTurn(box) {
 }
 
 function checkVictory() {
-  return;
+  // check rows
+  for (let row in [0, 1, 2]) {
+    if (
+      claimedGridArray[row][0] > 0 &&
+      claimedGridArray[row][0] == claimedGridArray[row][1] &&
+      claimedGridArray[row][1] == claimedGridArray[row][2]
+    ) {
+      return true;
+    }
+  }
+  for (let col in [0, 1, 2]) {
+    if (
+      claimedGridArray[0][col] > 0 &&
+      claimedGridArray[0][col] == claimedGridArray[1][col] &&
+      claimedGridArray[1][col] == claimedGridArray[2][col]
+    ) {
+      return true;
+    }
+  }
+
+  if (
+    claimedGridArray[0][0] > 0 &&
+    claimedGridArray[0][0] == claimedGridArray[1][1] &&
+    claimedGridArray[1][1] == claimedGridArray[2][2]
+  ) {
+    return true;
+  }
+  if (
+    claimedGridArray[0][2] > 0 &&
+    claimedGridArray[0][2] == claimedGridArray[1][1] &&
+    claimedGridArray[1][1] == claimedGridArray[2][0]
+  ) {
+    return true
+  }
+    return false;
 }
